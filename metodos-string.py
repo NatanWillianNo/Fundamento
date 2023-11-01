b28c2e5b14e8ab4fdedfe49817637479585042f3
NatanWillianNo <natanwilliannoronha2015@gmail.com>
2023-11-01 11:21:54 -0300

    Estudo de métodos de Strings, tipos primitivos e formatação de strings

---
+++ metodos-string.py
@@ -0,0 +1,14 @@
+continente = 'Africano'
+pais = 'Moçambique'
+print('O país {} está localizado no continente {}'.format(pais, continente))
+
+estado = 'São Paulo'
+time = 'Corinthians'
+print('O time {} está localizado no estado de {}'.format(time, estado))
+
+print('Haverá uma partida essa noite: será {} contra a seleção de {}! O maior time de {} contra o time da primeira divisão do continente {}'.format(time, pais, estado, continente))
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('DIgite outro: '))
+s = n1+n2
+print('A Soma entre {} e {} é {}'.format(n1, n2, s))
