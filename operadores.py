6b8d4ffaf7f147aabb6df460d087ffd07f4c9104
NatanWillianNo <natanwilliannoronha2015@gmail.com>
2023-11-01 12:25:32 -0300

    Estudo dos operadores: Aritméticos, atribuição e comparação

---
+++ operadores.py
@@ -0,0 +1,29 @@
+## Operadores aritméticos
+print(10-5)
+print(10*5)
+print(10/5)
+print(10**5)
+print(12//5)
+print(12%2)
+#Operadores de atribuição
+numero = int(input('Coloque um número aqui:  '))
+numero += 5 #Equivalente a `numero = numero + 5`
+print(numero) 
+numero -= 2 #Equivalente a `numero = numero - 2`
+print(numero) 
+numero *= 2 #Equivalente a `numero = numero * 2`
+print(numero) 
+numero /= 2 #Lembre-se que toda divisão gera um tipo de dado `float` #Equivalente a `numero = numero / 2`
+print(numero)
+numero **= 3  # Eleva o número à terceira potência e atribui o resultado a 'numero'
+print(numero)
+#Operadores de comparação
+numero1 = int(input('Coloque um número:'))
+numero2 = int(input('Coloque um número:'))
+print(numero1 > numero2)
+print(numero1 < numero2)
+print(numero1 == numero2)
+print(numero1 != numero2)
+print(numero1 >= numero2)
+print(numero1 <= numero2)
+# Operadores lógicos ou booleanos
