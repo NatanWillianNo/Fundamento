144183839fb11d17f6c1ef6be58ca92698a5d1a5
NatanWillianNo <natanwilliannoronha2015@gmail.com>
2023-12-01 10:25:14 -0300

    Exercícios Python - Curso em vídeo(ex089) + Exercícios Avulsos

--- exercicios-python-3/exercicios-python-gustavo-guanabara-ex089.py
+++ exercicios-python-3/exercicios-python-gustavo-guanabara-ex089.py
@@ -53,14 +53,17 @@ print("Valores ímpares em ordem crescente:", sorted(impares))
 
 # Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. 
 # No final, mostre a matriz na tela, com a formatação correta.
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for i in range(3):
-    for j in range(3):
-        matriz[i][j] = int(input(f"Digite o valor para a posição [{i + 1}, {j + 1}]: "))
-print("Matriz:")
-for i in range(3):
-    for j in range(3):
-        print(f"[{matriz[i][j]:^5}]", end='')
+matriz = []
+for linha in range(3):
+    valor = []
+    for coluna in range(3):
+        valor.append(int(input(f'Informe o valor para [{linha}, {coluna}]: ')))
+    matriz.append(valor)
+print()
+print('-' * 21 + f'\n{"MATRIZ":^22}\n' + '-' * 21)
+for linha in range(3):
+    for coluna in range(3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
     print()
 
 # Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. 
@@ -88,3 +91,34 @@ soma_terceira_coluna = sum(matriz[i][2] for i in range(3))
 print(f'B) A soma dos valores da terceira coluna é: {soma_terceira_coluna}')
 maior_segunda_linha = max(matriz[1])
 print(f'C) O maior valor da segunda linha é: {maior_segunda_linha}')
+
+# Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. 
+# No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
+
+sistema_de_notas = []
+alunos = []
+while True:
+    nome = input("Digite o nome do aluno (ou 'sair' para encerrar): ")
+    if nome.lower() == 'sair':
+        break
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    alunos.append([nome, nota1, nota2])
+print("\n=== Boletim ===")
+for aluno in alunos:
+    nome = aluno[0]
+    notas = aluno[1:]
+    media = sum(notas) / len(notas)
+    print(f"{nome}: Média = {media:.2f}")
+aluno_escolhido = input("\nDigite o nome do aluno para ver as notas individuais (ou 'sair' para encerrar): ")
+while aluno_escolhido.lower() != 'sair':
+    encontrado = False
+    for aluno in alunos:
+        if aluno[0].lower() == aluno_escolhido.lower():
+            encontrado = True
+            print(f"\nNotas de {aluno_escolhido}: {aluno[1:]}")
+            break
+    if not encontrado:
+        print(f"\nAluno '{aluno_escolhido}' não encontrado.")
+    aluno_escolhido = input("\nDigite o nome do aluno para ver as notas individuais (ou 'sair' para encerrar): ")
+
