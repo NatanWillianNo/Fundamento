94a6b9ed66bb0f1182546fc3c15162f3e43f4a53
NatanWillianNo <natanwilliannoronha2015@gmail.com>
2023-12-07 12:27:18 -0300

    Adiciona tags HTML para identificar cada tipo de informação no output da função extrair_informacoes_artigo. As informações agora são impressas com as seguintes tags: <strong>Título</strong>, <strong>Link</strong>, <strong>Data</strong>, <strong>Horário</strong>, <strong>Número da Nota</strong>, <strong>Categorias</strong>, <strong>Parágrafos</strong>.

--- reunioes/0512.py
+++ reunioes/0512.py
@@ -1,32 +1,54 @@
 from tinydb import TinyDB, Query
 from dotenv import load_dotenv
-import requests
 from bs4 import BeautifulSoup
+from unidecode import unidecode
+import requests
 import os
+
+titulo_lista = []
+link_lista = []
+data_lista = []
+horario_lista = []
+numero_da_nota_lista = []
+paragrafos_lista = []
+categoria_lista = []
+
 
 def acessar_pagina(url):
     html = requests.get(url)
-    bs = BeautifulSoup(html.text,'html.parser')
+    bs = BeautifulSoup(html.text, 'html.parser')
     http_code = html.status_code
     return bs, http_code
 
+#TODO Extrair data, horário e número da nota, parágrafos, categorias (OBS: Colocar com tag)
+#TODO Atualizar o banco com as informações acima 
+
+# Função para extrair informações de um artigo
 def extrair_infos():
     url = "https://www.gov.br/mre/pt-br/canais_atendimento/imprensa/notas-a-imprensa?b_start:int=30"
     bs = acessar_pagina(url)[0]
-    # <div id="content-core">
-    # <article class="tileItem visualIEFloatFix tile-collective-nitf-content">
     lista_tag_article = bs.find('div', attrs={'id': 'content-core'}).find_all('article', attrs={'class': 'tileItem visualIEFloatFix tile-collective-nitf-content'})
     for tag_article in lista_tag_article:
         titulo = tag_article.h2.text.strip()
+        titulo_lista.append(titulo)
         link = tag_article.h2.a['href'].strip()
-        #TODO Extrair data, horário e número da nota, parágrafos, categorias (OBS: Colocar com tag)
-        #TODO Atualizar o banco com as informações acima 
-        print(titulo)
-        print(link)
-        print('-'*40)
-        inserir_bd(titulo, link)
+        link_lista.append(link)
+        data = tag_article.find('i', class_='icon-day').parent.text.strip()
+        data_lista.append(data)
+        horario = tag_article.find('i', class_='icon-hour').parent.text.strip()
+        horario_lista.append(horario)
+        numero_da_nota = tag_article.find('span', class_='subtitle').text.strip().replace('NOTA À IMPRENSA ', '')
+        numero_da_nota_lista.append(horario)
+        artigo = acessar_pagina(link)
+        paragrafos = artigo[0].find('div', property='rnews:articleBody')
+        lista_paragrafos = paragrafos.find_all('p')
+        for p in lista_paragrafos:
+            p = p.text.strip()
+            print(p)
+        categorias = artigo[0].find('div', class_='contenttree-widget relationchoice-field').text
+        
 
-
+'''
 def inserir_bd(titulo, link): 
     env_dir = load_dotenv('.env_dir')
     DIR_DADOS_FINAL = os.getenv('DIR_DADOS_FINAL')
@@ -44,7 +66,7 @@ def inserir_bd(titulo, link):
     else:
         print('Já está na base!')
     # bd = TinyDB('{}/coleta.json'.format(DIR_DADOS_FINAL))
-
+'''
 def main():
     extrair_infos()
     # inserir_bd()
