67df35528c942c94d55a0f34d34f4a85a91fd515
NatanWillianNo <natanwilliannoronha2015@gmail.com>
2024-02-07 11:05:35 -0300

    Exercícios Python - Curso em vídeo(ex090 - ex091)

--- exercicios-python-3/exercicios-python-gustavo-guanabara-ex095.py
+++ exercicios-python-3/exercicios-python-gustavo-guanabara-ex095.py
@@ -49,6 +49,6 @@ while True:
     jogar_novamente = input("Deseja jogar novamente? (s/n): ").lower()
     if jogar_novamente != 's':
         break
-
+ 
 # Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 
